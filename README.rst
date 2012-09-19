1a602ad8aeea4e6cd2de2df70d2239020ba7cd8b
Aaron Iles <aaron.iles@gmail.com>
2012-09-19 21:57:05 +1000

    Use WordNet database to generate random slugs
    A random verb and noun is chosen from the Princeton WordNet database for
    each request to generate a new slug line.

--- README.rst
+++ README.rst
@@ -1,1 +1,4 @@
 Heroku application for www.twoslug.me
+
+Generates random two word slug lines with words from the Princeton WordNet
+database.
--- twoslug.py
+++ twoslug.py
@@ -1,17 +1,40 @@
 import os
+import random
+
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
-def random_slug():
-    return ''
+class WordNet(object):
+
+    def __init__(self, path='.'):
+        self.verbs = tuple(self.load_words(path, 'verb'))
+        self.nouns = tuple(self.load_words(path, 'noun'))
+
+    def load_words(self, path, category):
+        words = set()
+        filename = os.path.join(path, 'index.' + category)
+        with open(filename) as data:
+            for line in data:
+                word = line.split(None, 1)[0]
+                if not word.isalpha():
+                    continue
+                words.add(word)
+        return words
+
+    def random_slug(self):
+        verb = random.choice(self.verbs)
+        noun = random.choice(self.nouns)
+        return "{0} {1}".format(verb, noun)
+
+wordnet = WordNet('data')
 
 
 @app.route('/')
 def index():
-    return random_slug()
+    return wordnet.random_slug()
 
 
 if __name__ == '__main__':
