e666c1df2bd95f231205f6e9808b01569db38cd4
Aaron Iles <aaron.iles@gmail.com>
2013-09-08 22:43:49 +1000

    Create simple applicatoin runner
    Add simple script with command line options for launching the TwoSlug
    application using the development server.

---
+++ run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import begin
+from twoslug import app
+
+@begin.start
+@begin.convert(port=int, debug=begin.utils.tobool)
+def main(host='127.0.0.1', port=8000, debug=False):
+    app.run(host=host, port=port, debug = True)
