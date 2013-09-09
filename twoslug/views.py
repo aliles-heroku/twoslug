aae4f47a6d78094545e1a7dc8bd07672e7360130
Aaron Iles <aaron.iles@gmail.com>
2013-09-09 23:20:25 +1000

    Create an API for TwoSlug
    API endpoint returns a JSON object with the random words chosen for the
    slugline. The client may choose how many words and what word class they
    come from.

--- twoslug/views.py
+++ twoslug/views.py
@@ -1,6 +1,6 @@
 import random
 
-from flask import render_template
+from flask import abort, jsonify, render_template
 
 from twoslug import app
 from twoslug import wordnet
@@ -12,3 +12,17 @@ def index():
     noun = random.choice(wordnet.get_nouns())
     slug = '{0} {1}'.format(verb, noun)
     return render_template('index.html', slug=slug, title='TwoSlug')
+
+@app.route('/api/<path:path>/')
+def api(path):
+    words = []
+    for category in path.split('/'):
+        try:
+            word = {
+                    'class': category,
+                    'word': random.choice(wordnet.get_words(category))
+            }
+            words.append(word)
+        except IOError:
+            abort(400)
+    return jsonify(slugline=words)
