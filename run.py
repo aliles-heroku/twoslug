e5ee63f174c0d4e62678ac1ff8d6b392496dc38d
Aaron Iles <aaron.iles@gmail.com>
2014-04-04 20:46:34 +1100

    Add web and markov subcommands to run script
    Create separate subcommands for running the web application or
    generating a list of random wordoids.

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 import begin
-from twoslug import app
+from twoslug import app, model
 
-@begin.start
+@begin.subcommand
+@begin.convert(length=int, chain=int, words=int)
+def markov(category='noun', length=10, chain=3, words=1):
+    with app.app_context():
+        for _ in xrange(words):
+            print model.markov.generate_word(category, length, chain)
+
+@begin.subcommand
 @begin.convert(port=int)
-def main(host='local', port=8000, debug=False):
+def web(host='local', port=8000, debug=False):
     app.config['SERVER_NAME'] = '%s:%d' % (host, port)
     app.run(host=host, port=port, debug=debug)
+
+@begin.start
+def main():
+    pass
